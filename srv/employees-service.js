0343b01011d68b77e3a45026906e4d8a79de5ba5
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2020-04-25 14:10:36 +0200

    Bugfix employees-service

--- srv/employees-service.js
+++ srv/employees-service.js
@@ -11,22 +11,5 @@ module.exports = (srv) => {
     if (hasDuplicateProjects) {
       return req.error(410, "Duplicate projects are not allowed.");
     }
-
-    req.data.leaves.forEach(
-      (leave) => (leave.daysOfLeave = getDaysBetween(leave))
-    );
-
-    req.data.travels.forEach(
-      (travel) => (travel.daysOfTravel = getDaysBetween(travel))
-    );
   });
 };
-
-function getDaysBetween({ dateFrom, dateTo }) {
-  const oneDay = 24 * 60 * 60 * 1000;
-  const firstDate = new Date(dateFrom);
-  const secondDate = new Date(dateTo);
-  const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
-
-  return Number.isInteger(diffDays) ? diffDays : 0;
-}
