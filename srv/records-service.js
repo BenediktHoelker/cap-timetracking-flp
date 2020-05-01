92d864f39635c2a3ff08fc68a311acf6d481cb36
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2020-05-01 13:36:26 +0200

    Populate custom defaults for drafts

--- srv/records-service.js
+++ srv/records-service.js
@@ -1,10 +1,14 @@
-// module.exports = (srv) => {
-// srv.before(["UPDATE"], "Records", (req) => {
-//   if (req.data.status_ID !== "INITIAL") {
-//     return req.error(
-//       410,
-//       "The record has already been billed => Editing ist not allowed."
-//     );
-//   }
-// });
-// };
+module.exports = (srv) => {
+  srv.before("UPDATE", "Records", (req) => {
+    if (req.data.status_ID !== "INITIAL") {
+      req.error(
+        410,
+        "The record has already been billed => Editing ist not allowed."
+      );
+    }
+  });
+
+  srv.before("NEW", "Records", (req) => {
+    req.data.employee_ID = "9f5f70eb-3343-48e7-a643-2829096ca544";
+  });
+};
